19cc777214e1f4cf765d0a454e562d6bf954d6a7
harsh044 <hp1004032@gmail.com>
2023-03-26 17:49:00 +0530

    feat: code work properly

--- AI_assistant/questions.py
+++ AI_assistant/questions.py
@@ -1,34 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pyttsx3 as p
-class questions():
-    def __init__(self):
-        self.driver = webdriver.Edge(executable_path="C:\selenium\msedgedriver.exe")
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-    def question(self,query):
-        self.query = query
-        self.driver.get(url="https://www.answers.com/")
-        search = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div/div[1]/form/div/div/div/div/input')
-        search.click()
-        search.send_keys(query + Keys.ENTER)
+def question(query):
+    query = query
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+    
+    driver.get(url="https://www.answers.com/")
+    search = driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div/div[1]/form/div/div/div/div/input')
+    search.click()
+    search.send_keys(query + Keys.ENTER)
 
-        enter = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div[2]/div[3]/div')
-        enter.click()
+    enter = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div[2]/div[3]/div')
+    enter.click()
 
-        ans = self.driver.find_element_by_xpath('//*[@id="answer_58555"]/div[3]/div/p')
-        text1= ans.text
-        engine = p.init()
-        engine.say(text1)
-        engine.runAndWait()
-
-        # enter = self.driver.find_element_by_id("img")
-        # enter.click()
-
-        # info = self.driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[3]/ul/li[1]')
-        # text = info.text
-        # engine=p.init()
-        # engine.say(text)
-        # engine.runAndWait()
-
-# bot = questions()
-# bot.question("What is Internet?")
+    ans = driver.find_element_by_xpath('//*[@id="answer_58555"]/div[3]/div/p')
+    text1= ans.text
+    engine = p.init()
+    engine.say(text1)
+    engine.runAndWait()
